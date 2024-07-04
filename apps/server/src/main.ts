425a0ca8c15d849b9fd3324704fb7e722a429729
leostar <hi@leostar.top>
2024-07-04 16:26:24 +0800

    🔧 optimized engineering framework

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './app/app.module';
+import { appGlobalMiddleware } from './app/useGlobal';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: [
+      /^http:\/\/localhost(:\d+)?$/,
+      /^http:\/\/poster-craft\.leostar\.top(:81)?$/,
+    ],
+  });
+
+  appGlobalMiddleware(app);
+  const config = new DocumentBuilder()
+    .setTitle('PosterCraft Swagger')
+    .setDescription('The PosterCraft API description')
+    .setVersion('v1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+  await app.listen(process.env.PORT || 3001);
 }
+
 bootstrap();
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,12 +1,1 @@
-import { Client } from "@planetscale/database";
-import { drizzle } from "drizzle-orm/planetscale-serverless";
-
-const client = new Client({
-  host: process.env["MYSQL_DATABASE_HOST"],
-  username: process.env["MYSQL_DATABASE_USERNAME"],
-  password: process.env["MYSQL_DATABASE_PASSWORD"],
-});
-
-const db = drizzle(client);
-
-export default db;
+export { db } from "./db";
