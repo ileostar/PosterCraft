c1cc1209a35714252fdf69c6c989f902a676ab71
kkkang <779455866@qq.com>
2024-07-09 23:57:10 +0800

    💄 add main module UI

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,22 @@
 "use client";
 
+import Body_1 from "../components/main/body_1";
+import Body_2 from "../components/main/body_2";
+import Body_3 from "../components/main/body_3";
+import Footer from "../components/main/footer";
+import Head from "../components/main/head";
+
 function Main() {
-  return <div>123</div>;
+  return (
+    <div className="h-screen">
+      <Head />
+      <Body_1 />
+      <Body_2 />
+      <div className="divider w-4/5 mx-auto"></div>
+      <Body_3 />
+      <Footer />
+    </div>
+  );
 }
 
 export default Main;
