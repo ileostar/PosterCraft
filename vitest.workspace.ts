bda4b30e2605a795c1f5d8c5396442b1c34095a8
leostar <hi@leostar.top>
2024-07-12 14:14:17 +0800

    🚑 fix type error

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,9 +1,3 @@
 import { defineWorkspace } from "vitest/config";
 
-export default defineWorkspace([
-  "apps/server",
-  "apps/web",
-  "packages/db",
-  "packages/shared",
-  "packages/ui",
-]);
+export default defineWorkspace(["apps/web", "packages/db", "packages/shared", "packages/ui"]);
