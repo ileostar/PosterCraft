5ce40b2fbf5bcbc19e618bea7a09687503134f72
ileostar <hi@leostar.top>
2024-09-02 18:33:52 +0800

    ⚡️ add slip damping

--- apps/web/components/shared/ModelContainer.tsx
+++ apps/web/components/shared/ModelContainer.tsx
@@ -26,24 +26,16 @@ const ModelContainer: React.FC = () => {
         className="flex-1 md:items-center md:justify-center max-w-[500px] max-h-[500px] w-4/5 h-full hidden md:flex xl:min-w-[40vw] rounded-full overflow-hidden"
         ref={boxRef}
       >
-        <Suspense
-          fallback={
-            <div className="flex justify-center items-center h-full">
-              <Loading />
-            </div>
-          }
-        >
-          {dimensions.width && dimensions.height ? (
-            <ThreeModel
-              boxWidth={dimensions.width}
-              boxHeight={dimensions.height}
-            />
-          ) : (
-            <div className="flex justify-center items-center h-full">
-              <Loading />
-            </div>
-          )}
-        </Suspense>
+        {dimensions.width && dimensions.height ? (
+          <ThreeModel
+            boxWidth={dimensions.width}
+            boxHeight={dimensions.height}
+          />
+        ) : (
+          <div className="flex justify-center items-center h-full">
+            <Loading />
+          </div>
+        )}
       </div>
     </div>
   );
