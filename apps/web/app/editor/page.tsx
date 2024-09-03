aa5208e2b273b0af8d2c2905abf72624add31560
ileostar <hi@leostar.top>
2024-09-03 23:50:25 +0800

    ♻️ refactor editor ui

--- apps/web/app/editor/page.tsx
+++ apps/web/app/editor/page.tsx
@@ -1,19 +1,15 @@
-import AuthLayout from "@/components/layouts/AuthLayout";
+import EditorLayout from "@/components/layouts/EditorLayout";
 import Left from "@/components/pages/editor/Left/index";
 import Middle from "@/components/pages/editor/Middle";
 import Right from "@/components/pages/editor/Right/index";
 
 function Main() {
   return (
-    <AuthLayout>
-      <div className="h-screen flex flex-col">
-        <div className="flex h-full">
-          <Left />
-          <Middle />
-          <Right />
-        </div>
-      </div>
-    </AuthLayout>
+    <EditorLayout>
+      <Left />
+      <Middle />
+      <Right />
+    </EditorLayout>
   );
 }
 
