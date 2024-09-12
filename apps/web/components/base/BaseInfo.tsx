ecce8faf3684a29736798dfca32705222684c90b
ileostar <hi@leostar.top>
2024-09-13 03:28:06 +0800

    💄 beautify header

--- apps/web/components/base/BaseInfo.tsx
+++ apps/web/components/base/BaseInfo.tsx
@@ -8,14 +8,14 @@ interface BaseInfoProps {
 
 const BaseInfo: React.FC<BaseInfoProps> = ({ icon, title, content }) => {
   return (
-    <div className="flex items-center justify-center backdrop-blur-3xl  dark:backdrop-blur-xl bg-gray-300/30 dark:bg-gray-300/20 rounded-xl h-full p-5">
-      {icon && (
-        <span className={cn("text-rose-400 dark:text-[#FF33DE] h-16 w-16 rounded-full", icon)} />
-      )}
-      <div className="flex flex-col justify-center p-5">
+    <div className="flex items-center justify-center backdrop-blur-3xl hover:brightness-90 transition-all cursor-pointer group dark:bg-gradient-to-tl dark:from-gray-900/80 dark:to-gray-950/80 dark:hover:from-gray-800/80 dark:hover:to-gray-950/80 border-r-2 border-t-2 dark:border-gray-900/80 border-solid overflow-hidden relative dark:backdrop-blur-xl bg-gray-300/30 dark:bg-gray-300/10 rounded-xl h-full p-5 pb-0 card">
+      {icon && <span className={cn("text-rose-400 dark:text-[#FF33DE] h-16 w-16", icon)} />}
+      <div className="flex flex-col justify-center p-5 mb-5">
         <h3 className="text-xl font-bold">{title}</h3>
         <span className="text-sm text-gray-500 dark:text-white/60">{content}</span>
       </div>
+      <div className="h-2 w-full bg-gradient-to-l via-yellow-500 group-hover:blur-xl blur-2xl m-auto rounded transition-all absolute bottom-0"></div>
+      <div className="h-0.5 group-hover:w-full bg-gradient-to-l  via-yellow-950 group-hover:via-yellow-500 w-[70%] m-auto rounded transition-all"></div>
     </div>
   );
 };
--- apps/web/components/layouts/common/Header.tsx
+++ apps/web/components/layouts/common/Header.tsx
@@ -4,6 +4,7 @@ import SideMenu from "@/components/layouts/common/SideMenu";
 import Feature from "@/components/shared/Feature";
 import BaseMenu from "@/components/shared/Menu";
 import { useScroll } from "@/hooks/useScroll";
+import { cn } from "@/lib/utils";
 
 interface HeaderProps {
   scroll?: boolean;
@@ -14,7 +15,11 @@ const Header: React.FC<HeaderProps> = ({ className, scroll = false }) => {
   const scrolled = useScroll(100);
   return (
     <header
-      className={`sticky top-0 z-40 w-[100vw-1.5] max-h-[20vh] backdrop-blur-xl transition-all mx-auto ${className} ${scroll ? (scrolled ? "border-b" : "bg-transparent") : "border-b"}`}
+      className={cn(
+        "sticky top-0 z-40 w-[100vw-1.5] max-h-[20vh] backdrop-blur-xl transition-all mx-auto",
+        `${scroll ? (scrolled ? "border-b" : "bg-transparent") : "border-b"}`,
+        className,
+      )}
     >
       <nav className="mx-auto max-w-7xl min-[845px]:py-4 transition-all justify-between">
         <div className="w-full mx-4 sm:mx-8 md:mx-12 xl:mx-0 flex justify-between">
